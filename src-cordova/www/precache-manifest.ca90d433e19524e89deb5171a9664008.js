e082e84c1bcf7a0a8179ddd20b39b587020702f8
Bestian Tang <bestian@gmail.com>
2019-06-29 13:19:23 +0800

    v1.2

---
+++ src-cordova/www/precache-manifest.ca90d433e19524e89deb5171a9664008.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9d6a644884f9c0f39bac",
+    "url": "css/app.92a4e243.css"
+  },
+  {
+    "revision": "a1bdc8c0834045d51b9c",
+    "url": "css/chunk-vendors.dc1cd0c8.css"
+  },
+  {
+    "revision": "a1bdc8c0834045d51b9c",
+    "url": "js/chunk-vendors.2641faa3.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "img/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "c33eefd5e4fc4fc0b4f9",
+    "url": "js/about.169d6569.js"
+  },
+  {
+    "revision": "9d6a644884f9c0f39bac",
+    "url": "js/app.98d89cb5.js"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "34b12f8045024facc4dce2b6d0119e5e",
+    "url": "index.html"
+  },
+  {
+    "revision": "c33eefd5e4fc4fc0b4f9",
+    "url": "css/about.953787f3.css"
+  }
+];
